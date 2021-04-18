5522e6bc090d53510784f3034341a50327240cc6
cartman1978 <cartman_7802@yahoo.ie>
2021-04-18 18:23:40 +0000

    added about.html page

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
